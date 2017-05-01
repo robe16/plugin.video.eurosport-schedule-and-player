0f988eb96c3e34fb0420ea7aa22279b62491c914
Rob <robe16@hotmail.co.uk>
2017-05-01 18:00:52 +0100

    Addition of thumb image for EPG items (source from eurosport schedule xml) and additional log messages created for xml request and try/except on producing GUI of listings.

--- resources/lib/epg.py
+++ resources/lib/epg.py
@@ -1,6 +1,7 @@
 from resources.lib.items import Items
 import datetime
 import ast
+from resources.lib.common import *
 import resources.lib.simple_requests as requests
 from resources.lib.bs4 import BeautifulSoup
 
@@ -10,59 +11,66 @@ ESLISTINGS_URL = "http://uk.eurosportplayer.com/tvschedule.xml"
 
 def listings():
     #
-    listings = getListing()
-    #
-    if listings:
-        soup = BeautifulSoup(listings, 'html.parser')
+    try:
         #
-        li_all = soup.findAll("li", {"class": "tvschedule-main__right-program"})
+        listings = getListing()
         #
-        proglist = {}
-        #
-        for li in li_all:
+        if listings:
+            soup = BeautifulSoup(listings, 'html.parser')
             #
-            dateformat = '%Y-%m-%dT%H:%M:%S'
-            dt_start = datetime.datetime.strptime(li.attrs['data-startdate'], dateformat)
-            dt_end = datetime.datetime.strptime(li.attrs['data-enddate'], dateformat)
+            li_all = soup.findAll("li", {"class": "tvschedule-main__right-program"})
             #
-            if dt_start >= datetime.datetime.now() or dt_end >= datetime.datetime.now():
+            proglist = {}
+            #
+            for li in li_all:
                 #
-                if not 'no-prog' in li.attrs['class']:
+                dateformat = '%Y-%m-%dT%H:%M:%S'
+                dt_start = datetime.datetime.strptime(li.attrs['data-startdate'], dateformat)
+                dt_end = datetime.datetime.strptime(li.attrs['data-enddate'], dateformat)
+                #
+                if dt_start >= datetime.datetime.now() or dt_end >= datetime.datetime.now():
                     #
-                    data_prog = li.attrs['data-prg']
-                    data_prog = ast.literal_eval(data_prog)
-                    #
-                    channel_id = data_prog['channelid']
-                    title_full = data_prog['sporteventname']
-                    title_short = data_prog['shortname']
-                    desc = data_prog['description']
-                    #
+                    if not 'no-prog' in li.attrs['class']:
+                        #
+                        data_prog = li.attrs['data-prg']
+                        data_prog = ast.literal_eval(data_prog)
+                        #
+                        channel_id = data_prog['channelid']
+                        title_full = data_prog['sporteventname']
+                        title_short = data_prog['shortname']
+                        desc = data_prog['description']
+                        img = data_prog['data-channelimg']
+                        #
 
-                    if data_prog['transmissiontype'] == '1':
-                        live = ' [COLOR red]LIVE[/COLOR]'
-                    else:
-                        live = ''
-                    #
-                    dict_key = '{start_time}_{channel_id}'.format(start_time=dt_start.strftime('%Y-%m-%d_%H:%M'),
-                                                                  channel_id=channel_id)
-                    #
-                    proglist[dict_key] = {'channel_id': channel_id,
-                                          'title_full': title_full,
-                                          'title_short': title_short,
-                                          'desc': desc,
-                                          'live': live,
-                                          'dt_start': dt_start,
-                                          'dt_end': dt_end}
-                    #
-        for key in sorted(proglist.iterkeys()):
-            #
-            item_label = '{time}{live} {title}'.format(time=proglist[key]['dt_start'].strftime("%d-%m-%Y %H:%M"),
-                                                       live=proglist[key]['live'],
-                                                       title=proglist[key]['title_full'])
-            #
-            items.add({'mode': '#', 'title': item_label})
-    #
-    items.list()
+                        if data_prog['transmissiontype'] == '1':
+                            live = ' [COLOR red]LIVE[/COLOR]'
+                        else:
+                            live = ''
+                        #
+                        dict_key = '{start_time}_{channel_id}'.format(start_time=dt_start.strftime('%Y-%m-%d_%H:%M'),
+                                                                      channel_id=channel_id)
+                        #
+                        proglist[dict_key] = {'channel_id': channel_id,
+                                              'title_full': title_full,
+                                              'title_short': title_short,
+                                              'desc': desc,
+                                              'live': live,
+                                              'img': img,
+                                              'dt_start': dt_start,
+                                              'dt_end': dt_end}
+                        #
+            for key in sorted(proglist.iterkeys()):
+                #
+                item_label = '{time}{live} {title}'.format(time=proglist[key]['dt_start'].strftime("%d-%m-%Y %H:%M"),
+                                                           live=proglist[key]['live'],
+                                                           title=proglist[key]['title_full'])
+                #
+                items.add({'mode': '#', 'title': item_label, 'thumb': proglist[key]['img']})
+        #
+        items.list()
+        #
+    except Exception as e:
+        log('Eurosportplayer: ERROR attempting to retrieve and present tv schedule - {error}'.format(error=e))
 
 def getListing():
     #
@@ -71,4 +79,5 @@ def getListing():
     if r.status_code == 200:
         return r.text
     else:
+        log('Eurosportplayer: ERROR attempting to retrieve - status code {code} returned'.format(code=r.status_code))
         return False
